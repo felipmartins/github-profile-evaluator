a8992760747ea1a6fa21ec5c265117bc3178105a
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-09-06 16:57:40 -0300

    adds models

--- evaluator/models.py
+++ evaluator/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Tracker(models.Model):
+    name = models.CharField(max_length=25)
+    clicks = models.IntegerField(default=0)
+
+class Evaluation(models.Model):
+    github_user = models.CharField(max_length=50)
+    has_photo = models.BooleanField()
+    has_email = models.BooleanField()
+    has_linkedin = models.BooleanField()
+    has_readme = models.BooleanField()
+    stacks = models.IntegerField()
+    repositories = models.IntegerField()
+    pinned_repositories = models.IntegerField()
+    grade = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.github_user}: {self.grade}/100'
