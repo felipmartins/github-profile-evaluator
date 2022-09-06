5ee1034967e0f9994f1cffd880525103415cc884
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-09-06 18:29:35 -0300

    change files for devolpment

--- evaluator/views.py
+++ evaluator/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from evaluator.fetch import single_fetch_content
 from evaluator.data_getter import populate_dict
+from time import sleep
 
 def index(request):
     context = { 
@@ -9,9 +10,10 @@ def index(request):
 
     if request.method == 'POST':
         print(request.POST['github_user'])
-        print(populate_dict(single_fetch_content(request.POST['github_user'])))
 
         context = {
+
+            'git_user': populate_dict(single_fetch_content(request.POST['github_user']))
             
         }
     
