50f8b4e1d961335f7a52ebecab9e9e430c6fc9b2
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-09-09 14:01:09 -0300

    changes logic to find stacks

--- evaluator/readme_handler.py
+++ evaluator/readme_handler.py
@@ -6,9 +6,25 @@ def get_readme(selector):
 
 
 def get_tags(selector):
-    
+
+    stacks = [
+        "html", "css", "javascript", "jest", "react", "cypress",
+        "rtl", "react-testing-library", "redux", "docker", "sql",
+        "mongo", "node", "express", "mocha", "next.js", "typescript",
+        "jwt", "python", "csharp", "cpp", "postman", "nginx", "django",
+        "bash", "sequelize", "prisma", "bootstrap", "linux", "mac", 
+        "tailwind"
+    ]
+
     if selector == 404:
         return 0
     
-    print(selector.css("img").getall())
-    return len(selector.css("img").getall())
+    stack_counter = 0
+
+    for stack in stacks:
+        for img in selector.css("img").getall():
+            if stack in img:
+                stack_counter += 1
+                break
+    
+    return stack_counter
