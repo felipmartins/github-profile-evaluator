512b1541647e91960ca8bbce43a68ac14ccff124
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-09-10 19:16:19 -0300

    add models to group evaluation

--- evaluator/models.py
+++ evaluator/models.py
@@ -30,3 +30,36 @@ class Evaluation(models.Model):
 
     def __str__(self):
         return f'{self.github_user}: {self.grade}/100'
+
+
+class GroupCSV(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    file = models.FileField(upload_to='evaluator/static/csvs')
+
+    def __str__(self):
+        return str(self.file)
+
+
+class GroupEvaluation(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    csv_file = models.ForeignKey(GroupCSV, on_delete=models.CASCADE)
+    github_user = models.CharField(max_length=50)
+    evaluation_date = models.DateField(auto_now_add=True)
+    has_photo = models.BooleanField()
+    has_email = models.BooleanField()
+    has_linkedin = models.BooleanField()
+    stacks = models.IntegerField()
+    repositories = models.IntegerField()
+    pinned_repositories = models.IntegerField()
+    has_five_or_more_stacks = models.BooleanField()
+    has_ten_or_more_stacks = models.BooleanField()
+    has_five_or_more_repos = models.BooleanField()
+    has_ten_or_more_repos = models.BooleanField()
+    has_two_or_more_pinned = models.BooleanField()
+    has_four_or_more_pinned = models.BooleanField()
+    grade = models.IntegerField()
+    github_profile_image = models.ImageField()
+
+
+    def __str__(self):
+        return f'{self.github_user}: {self.grade}/100'
