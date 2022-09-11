6654b16ae3ff9bcbc30dd3f3f5a68b679583e8a9
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-09-10 22:14:12 -0300

    changes models features

--- evaluator/models.py
+++ evaluator/models.py
@@ -34,7 +34,7 @@ class Evaluation(models.Model):
 
 class GroupCSV(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    file = models.FileField(upload_to='evaluator/static/csvs')
+    file = models.FileField(upload_to='evaluator/static/csvs/')
 
     def __str__(self):
         return str(self.file)
