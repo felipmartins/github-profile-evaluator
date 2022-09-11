df441869c40e273178b7eb497e64eaab14abb988
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-09-10 22:15:11 -0300

    add CSVForms

--- evaluator/forms.py
+++ evaluator/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Evaluation
+from .models import Evaluation, GroupCSV
 
 
 class GitHubUsernameForm(forms.ModelForm):
@@ -7,3 +7,9 @@ class GitHubUsernameForm(forms.ModelForm):
     class Meta:
         model = Evaluation
         fields = ['github_user']
+
+class CSVForm(forms.ModelForm):
+
+    class Meta:
+        model = GroupCSV
+        fields = ['file']
