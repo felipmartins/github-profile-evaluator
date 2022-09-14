2f8867534ac7ed14d94a2a4212fe15ada3bc2c72
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-09-14 15:28:19 -0300

    condition to tracking

--- evaluator/tracker.py
+++ evaluator/tracker.py
@@ -3,8 +3,11 @@ from .models import Tracker
 
 
 def raise_evaluation_clicks():
-    tracker = get_object_or_404(Tracker, id=1)
+    if len(Tracker.objects.all()) > 0:
+        tracker = get_object_or_404(Tracker, id=1)
 
-    tracker.clicks += 1
+        tracker.clicks += 1
 
-    tracker.save()
+        tracker.save()
+    else:
+        ...
