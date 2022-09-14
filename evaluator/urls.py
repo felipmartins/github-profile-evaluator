e491587f6ebe64b72eadbdf2086eba5f60525ebb
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-09-14 09:23:55 -0300

    format urls.py

--- evaluator/urls.py
+++ evaluator/urls.py
@@ -3,9 +3,13 @@ from django.urls import path
 from .views import index, evaluation, group_evaluation, group_index, pdf_export
 
 urlpatterns = [
-    path('', index, name='homepage'),
-    path('group', group_index, name='group-homepage'),
-    path('evaluation/single/<str:uuid>', evaluation, name='evaluation'),
-    path('evaluation/group/<str:uuid>', group_evaluation, name='group-evaluation'),
-    path('evaluation/<str:type>/<str:uuid>/download', pdf_export, name='download-evaluation')
+    path("", index, name="homepage"),
+    path("group", group_index, name="group-homepage"),
+    path("evaluation/single/<str:uuid>", evaluation, name="evaluation"),
+    path("evaluation/group/<str:uuid>", group_evaluation, name="group-evaluation"),
+    path(
+        "evaluation/<str:type>/<str:uuid>/download",
+        pdf_export,
+        name="download-evaluation",
+    ),
 ]
