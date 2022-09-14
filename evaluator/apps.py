3493055db25dc248102be5f756cfd9e246b17336
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-09-14 09:20:48 -0300

    format apps.py

--- evaluator/apps.py
+++ evaluator/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class EvaluatorConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'evaluator'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "evaluator"
