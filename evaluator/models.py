5451de43088ba1d44a631a1953ab3ef8bc5ab4f4
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-09-16 15:19:10 -0300

    changes to deploy

--- evaluator/models.py
+++ evaluator/models.py
@@ -35,7 +35,7 @@ class Evaluation(models.Model):
 
 class GroupCSV(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    file = models.FileField(upload_to="evaluator/static/csvs/")
+    file = models.FileField(upload_to="evaluator/media/csvs/")
 
     def __str__(self):
         return str(self.file)
