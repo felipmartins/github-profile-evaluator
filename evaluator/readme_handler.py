5f4b60dcf86838e1a8a2fd9e334d478f08fe8587
Vitor Buxbaum <vitorbuxbaum@gmail.com>
2022-09-18 10:59:57 -0300

    whitespace minor fixes

--- evaluator/readme_handler.py
+++ evaluator/readme_handler.py
@@ -12,17 +12,17 @@ def get_tags(selector):
         "rtl", "react-testing-library", "redux", "docker", "sql",
         "mongo", "node", "express", "mocha", "next.js", "typescript",
         "jwt", "py", "csharp", "cpp", "postman", "nginx", "django",
-        "bash", "sequelize", "prisma", "bootstrap", "linux", "mac", 
+        "bash", "sequelize", "prisma", "bootstrap", "linux", "mac",
         "tailwind", "algoritmos", "dados", "redes", "objetos", "js"
     ]
 
     if selector == 404:
         return 0
-    
+
     stack_counter = 0
 
     for stack in stacks:
         if stack in selector.get().lower():
             stack_counter += 1
-    
+
     return stack_counter
