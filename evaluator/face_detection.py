7798c913f7ccd160a16630cf69cd0964d3ec569c
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-09-21 05:40:18 -0300

    refactor code considering all haarcascades in folder

--- evaluator/face_detection.py
+++ evaluator/face_detection.py
@@ -3,36 +3,41 @@ import cv2
 
 class FaceDetector:
 
-    resize_factor = 0.25
+        @classmethod
+        def find_faces(cls, path):
 
-    frontal = cv2.CascadeClassifier("evaluator/haarcascade_frontalface_default.xml")
+            resize_factor = 0.25
+            cascades = ["haarcascade_frontalface_default.xml","haarcascade_profileface.xml","haarcascade_frontalface_alt.xml","haarcascade_frontalface_alt2.xml","haarcascade_frontalface_alt_tree.xml","haarcascade_eye.xml"]
+            for cascade in cascades:
+                frontal = cv2.CascadeClassifier("evaluator/" + cascade)
+                print(cascade)
 
-    @classmethod
-    def find_faces(cls, path):
+                image = cv2.imread(path)
 
-        image = cv2.imread(path)
+                if image is None:
+                    return False
 
-        if image is None:
-            return False
+                reduced = cv2.resize(
+                    image,
+                    None,
+                    fx=resize_factor,
+                    fy=resize_factor,
+                    interpolation=cv2.INTER_NEAREST,
+                )
+                gray = cv2.cvtColor(reduced, cv2.COLOR_BGR2GRAY)
 
-        reduced = cv2.resize(
-            image,
-            None,
-            fx=cls.resize_factor,
-            fy=cls.resize_factor,
-            interpolation=cv2.INTER_NEAREST,
-        )
-        gray = cv2.cvtColor(reduced, cv2.COLOR_BGR2GRAY)
+                faces = frontal.detectMultiScale3(
+                    gray, scaleFactor=1.1, minNeighbors=3, outputRejectLevels=True
+                )
 
-        faces = cls.frontal.detectMultiScale3(
-            gray, scaleFactor=1.1, minNeighbors=3, outputRejectLevels=True
-        )
+                for dims in faces[0]:
+                    dims = (dims / resize_factor).round().astype(int)
+                    x, y, w, h = dims
+                    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-        for dims in faces[0]:
-            dims = (dims / cls.resize_factor).round().astype(int)
-            x, y, w, h = dims
-            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                cv2.imwrite(path, image)
 
-        cv2.imwrite(path, image)
+                print(faces)
 
-        return bool(len(faces[0]))
+                if bool(len(faces[0])):
+                    return bool(len(faces[0]))
