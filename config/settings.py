53de42e26faa9521bdc04e1aba3266edb8f42ea0
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-09-23 12:04:29 -0300

    add cors headers

--- config/settings.py
+++ config/settings.py
@@ -25,17 +25,21 @@ INSTALLED_APPS = [
     "evaluator",
     "rest_framework",
     "django_filters",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True if env("CORS_ALLOW_ALL_ORIGINS") == "True" else False
 
 ROOT_URLCONF = "config.urls"
 
