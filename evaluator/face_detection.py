662ba128f52202fcdfd96cd951190094c2f294dc
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-09-26 14:26:24 -0300

    refac: face_detection: remove resize

--- evaluator/face_detection.py
+++ evaluator/face_detection.py
@@ -5,7 +5,6 @@ class FaceDetector:
     @classmethod
     def find_faces(cls, path):
 
-        resize_factor = 0.25
         cascades = [
             "haarcascade_frontalface_default.xml",
             "haarcascade_profileface.xml",
@@ -22,22 +21,12 @@ class FaceDetector:
             if image is None:
                 return False
 
-            reduced = cv2.resize(
-                image,
-                None,
-                fx=resize_factor,
-                fy=resize_factor,
-                interpolation=cv2.INTER_NEAREST,
-            )
-            gray = cv2.cvtColor(reduced, cv2.COLOR_BGR2GRAY)
 
             faces = frontal.detectMultiScale3(
-                gray, scaleFactor=1.1, minNeighbors=3, outputRejectLevels=True
+                image, scaleFactor=1.1, minNeighbors=3, outputRejectLevels=True
             )
 
-            for dims in faces[0]:
-                dims = (dims / resize_factor).round().astype(int)
-                x, y, w, h = dims
+            for x, y, w, h in faces[0]:
                 cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
             cv2.imwrite(path, image)
