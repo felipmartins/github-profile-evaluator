bf3c184e33509f66b8805bb5950e66a206db165f
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-10-12 08:57:10 -0300

    remove unnecessary stuff

--- evaluator/views.py
+++ evaluator/views.py
@@ -49,7 +49,6 @@ def group_index(request):
             csv_object = GroupCSV(file=request.FILES["file"])
             csv_object.save()
             csv_list = csv_to_list("media/" + csv_object.file.name)
-            request
             if not check_usernamekey_in_csv(csv_object, csv_list):
                 request.session[
                     "erro"
